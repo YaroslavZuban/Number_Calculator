001747cfaf331c112c743062173b69fcc6ad0459
Yaroslav <yaroslav.zuban7029@gmail.com>
2023-04-11 16:38:55 +0700

    Добавил графический интерфейс

---
+++ src/main/java/com/example/number_calculator/ComplexController.java
@@ -0,0 +1,161 @@
+package com.example.number_calculator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class ComplexController {
+    @FXML
+    private Button buttonBackSpace;
+
+    @FXML
+    private ImageView buttonClose;
+
+    @FXML
+    private ImageView buttonCollapse;
+
+    @FXML
+    private ImageView buttonHistory;
+
+    @FXML
+    private ImageView buttonInfo;
+
+    @FXML
+    private Button button_0;
+
+    @FXML
+    private Button button_1;
+
+    @FXML
+    private Button button_2;
+
+    @FXML
+    private Button button_3;
+
+    @FXML
+    private Button button_4;
+
+    @FXML
+    private Button button_5;
+
+    @FXML
+    private Button button_6;
+
+    @FXML
+    private Button button_7;
+
+    @FXML
+    private Button button_8;
+
+    @FXML
+    private Button button_9;
+
+    @FXML
+    private Button button_A;
+
+    @FXML
+    private Button button_Addition;
+
+    @FXML
+    private Button button_B;
+
+    @FXML
+    private Button button_C;
+
+    @FXML
+    private Button button_D;
+
+    @FXML
+    private Button button_Division;
+
+    @FXML
+    private Button button_MC;
+
+    @FXML
+    private Button button_MPlus;
+
+    @FXML
+    private Button button_MR;
+
+    @FXML
+    private Button button_MS;
+
+    @FXML
+    private Button button_Multiplication;
+
+    @FXML
+    private Button button_OneDivisionX;
+
+    @FXML
+    private Button button_Point;
+
+    @FXML
+    private Button button_Pow_Two;
+
+    @FXML
+    private Button button_Result;
+
+    @FXML
+    private Button button_SignChange;
+
+    @FXML
+    private Button button_Subtraction;
+
+    @FXML
+    private TextField textInput;
+
+    @FXML
+    private TextField textResult;
+
+    @FXML
+    private AnchorPane windowMain;
+
+    @FXML
+    void actionOperator(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onBaskSpace(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onClean(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCleanEntry(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onMemory(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onNumberClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onOperatorClicking(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onResultClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onSymbolClicked(MouseEvent event) {
+
+    }
+
+}
